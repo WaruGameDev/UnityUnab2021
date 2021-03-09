74359d130270bd86882bc7568479e3560c891e29
WaruGameDev <walter.veneros@ulpomedia.com>
2021-03-09 10:24:03 -0300

    personaje responde al mouse

---
+++ Assets/Scripts/EjemploVariables.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjemploVariables : MonoBehaviour
+{
+    EjemploVariables ejemploVariables;
+
+    public string textoEjemplo = "hola";
+    public int numeroEnteroEjemplo = 55;
+    public float numeroDecimalEjemplo = 5.5f;
+    public bool verdaderoOFalso = true;
+
+    public void FuncionEjemplo()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ejemploVariables.FuncionEjemplo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/HP_Character.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP_Character : MonoBehaviour
+{
+    public string nameCharacter = "";
+    public int hp = 5;
+    bool rotando;
+    
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+        if(hp <= 0)
+        {
+            Destroy(gameObject);
+            print(nameCharacter + " ha muerto :c");
+        }
+    }
+    private void OnMouseDown()
+    {
+        TakeDamage(1);
+    }
+
+    private void Update()
+    {
+        if(rotando)
+        {
+            transform.Rotate(transform.forward * 5);
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        rotando = true;
+    }
+    private void OnMouseExit()
+    {
+        rotando = false;
+    }
+}
