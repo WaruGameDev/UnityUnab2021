66fa74512c83e3e1546c5a2cfd625004e7691d30
WaruGameDev <walter.veneros@ulpomedia.com>
2021-05-25 11:20:52 -0400

    clase 25 junio

---
+++ Assets/ShootFPSObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootFPSObject : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform canon;
+    public float potencia = 800;
+    
+    public void Shoot()
+    {
+        GameObject bulletGo = Instantiate(bullet, canon.position, Quaternion.identity);
+        bulletGo.GetComponent<Rigidbody>().AddForce(potencia * canon.transform.forward);
+    }
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+}
